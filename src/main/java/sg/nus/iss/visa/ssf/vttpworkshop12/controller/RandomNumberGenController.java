49f792c612784a1729c07641fd5683be1c20f661
Nilesh <nil.y.jadhav@gmail.com>
2023-05-16 14:55:25 +0800

    commit

--- src/main/java/sg/nus/iss/visa/ssf/vttpworkshop12/controller/RandomNumberGenController.java
+++ src/main/java/sg/nus/iss/visa/ssf/vttpworkshop12/controller/RandomNumberGenController.java
@@ -1,13 +1,24 @@
 package sg.nus.iss.visa.ssf.vttpworkshop12.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
+import sg.nus.iss.visa.ssf.vttpworkshop12.model.Image;
+import sg.nus.iss.visa.ssf.vttpworkshop12.service.RandomNumService;
 
 @Controller
 public class RandomNumberGenController {
+
+    //injecting service class depedency
+    @Autowired
+    RandomNumService service;
+
 
     @GetMapping("/home")
     public String langingPage(){
@@ -29,6 +40,22 @@ public class RandomNumberGenController {
             model.addAttribute("errorMessage", errorMessage);
             return "home";
         }
+
+        //calling service method to generate random numbers
+        List<Integer> randomNumbers = service.generateRanNumbers(number);
+
+
+        //populate image objects and create list of Image objects
+        List<Image> imageList = new ArrayList<Image>();
+
+       
+
+        for(int randomNumber : randomNumbers){
+            imageList.add(new Image(Integer.toString(randomNumber), "/images/"+Integer.toString(randomNumber)+".png"));
+        }
+        System.out.println("image list :" + imageList);
+
+        model.addAttribute(imageList);
 
         return "display";
     }
--- src/main/java/sg/nus/iss/visa/ssf/vttpworkshop12/service/RandomNumService.java
+++ src/main/java/sg/nus/iss/visa/ssf/vttpworkshop12/service/RandomNumService.java
@@ -1,8 +1,18 @@
 package sg.nus.iss.visa.ssf.vttpworkshop12.service;
 
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class RandomNumService {
+
+    public List<Integer> generateRanNumbers(int number) {
+
+        return new Random().ints(1, 31).distinct().limit(number).boxed().collect(Collectors.toList());
+
+    }
     
 }
