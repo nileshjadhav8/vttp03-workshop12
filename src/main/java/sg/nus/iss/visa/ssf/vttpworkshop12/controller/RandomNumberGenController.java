00ac50e2d2bef5c8e1749355d6b12e5928dbe260
Nilesh <nil.y.jadhav@gmail.com>
2023-05-16 14:30:17 +0800

    commit

--- src/main/java/sg/nus/iss/visa/ssf/vttpworkshop12/controller/RandomNumberGenController.java
+++ src/main/java/sg/nus/iss/visa/ssf/vttpworkshop12/controller/RandomNumberGenController.java
@@ -1,7 +1,10 @@
 package sg.nus.iss.visa.ssf.vttpworkshop12.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class RandomNumberGenController {
@@ -10,5 +13,25 @@ public class RandomNumberGenController {
     public String langingPage(){
         return "home";
     }
+
+    @GetMapping("/get")
+    public String generateRanNumbers(Model model, HttpServletRequest request){
     
+        //fetch input param
+        int number = Integer.parseInt(request.getParameter("number"));
+
+        System.out.println("input no is: " +number);
+
+        //check condition if no is between 0-30
+        if(number < 1 || number > 30){
+
+            String errorMessage = "Invalid Number: " +number;
+            model.addAttribute("errorMessage", errorMessage);
+            return "home";
+        }
+
+        return "display";
+    }
+    
+
 }
